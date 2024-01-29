d4cdadecbb2d8638f5ac074d4c28f7d997293ba2
zRinick <150109116+zRinick@users.noreply.github.com>
2024-01-29 21:02:15 +0700

    แก้codeเพิ่มเติม
    ขอบคุณครับ

--- LabSheet10/src/TestCustomer.java
+++ LabSheet10/src/TestCustomer.java
@@ -1,29 +1,36 @@
 
+
 public class TestCustomer {
+    public static void main(String[] arge) {
+        System.out.println("###Test Customer class###");
+        Customer c1 = new Customer(1088,"Conan Edogawa", 10);
+        Customer c2 = new Customer(1088,"Conan Edogawa", 5);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println("*************************************");
+        System.out.println("id is: "+c1.getId());
 
-	public static void main(String[] args) {
-		System.out.println("###Test Customer class###");
-		Customer c1 = new Customer(1088, "Conan Edogawa",10);
-		Customer c2 = new Customer(1088, "Conan Edogawa",5);
-		System.out.println(c1);
-		System.out.println(c2);
-		System.out.println("****************************");
-		System.out.println("id is: "+c1.getID());
-		System.out.println("name is: "+c1.getName());
-		System.out.println("discount is: "+c2.getDiscout());
-		
-		System.out.println("****************************");
-		System.out.println("###Test Invoice class###");
-		Invoice inv1 = new Invoice(101, c1,12000.00);
-		System.out.println();
-		
-		Line();
-		//ยังไม่เสร็จครับ 
-		//เดี่ยวมาทำต่อครับ
-		//อย่าว่าผมนะครับ
-	}
-	public static void Line() {
-		
-	}
+        System.out.println("name is: "+c1.getName());
 
+        System.out.println("discount is: "+c2.getDiscount());
+
+        System.out.println("***************************************");
+
+        System.out.println("###Test Invoice class##"); 
+        Invoice i1 = new Invoice(101, c2, 12000.00);
+        Invoice i2 = new Invoice(101,c2,9500);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println("***************************************");
+        System.out.println("id is: " + i1.getId());
+        System.out.println("customer is: " +i1.getCustomer());
+        System.out.println("amount is: " + i1.getAmountAfterDiscount());
+        System.out.println("**************************************");
+        System.out.println("customer's id is: " + i2.getCustomerID());
+        System.out.println("customer's name is: " + i2.getCustomerName());
+        System.out.println("customer's discount is: " + i2.getCustomerDiscount());
+        System.out.println("amount after discount is: " + i2.getAmountAfterDiscount()); 
+    }
+    
+    	  
 }
